056240c67917e296d464c2129990736cea4948f7
Jason Herrera <baytownjason@yahoo.com>
2021-10-06 18:37:30 -0500

    character bio alert

--- dbz-tribute/src/Card.js
+++ dbz-tribute/src/Card.js
@@ -4,24 +4,26 @@ import React, { useState, useEffect } from 'react'
 function Card({character}) {
 
     const [image, setImage] = useState("")
+    const [popup, setPopup] = useState("")
 
     useEffect(() => {
         setImage(Images[`${character.id}`])
     })
 
     const openCard = () => {
-        
+        setPopup(character.id)
+        window.alert(character.bio)
     }
 
 
 
     return (
-        <div className="card" onClick={openCard}>
+        <div className="card" id={character.id} onClick={openCard}>
             <h1 className="card__name">{character.name}</h1>
             <img src={image} alt="" className="card__img"/>
-            <p className="card__health">health: {character.health}</p>
             <p className="card__attack">attack: {character.attack}</p>
             <p className="card__defense">defense: {character.defense}</p>
+            <p className="card__health">health: {character.health}</p>
         </div>
     )
 }
