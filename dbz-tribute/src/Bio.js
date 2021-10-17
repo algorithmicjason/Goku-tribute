014b18fa9acb95a73ccdad7863bb009383529fad
Jason Herrera <baytownjason@yahoo.com>
2021-10-16 22:03:25 -0500

    bio componentstarted

---
+++ dbz-tribute/src/Bio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Bio = (props) => {
+    // debugger
+    // console.log(props.props.bio)
+    <div className="Bio__Popup">
+        {/* <h1>hello</h1> */}
+    </div>
+}
+export default Bio;
--- dbz-tribute/src/Card.js
+++ dbz-tribute/src/Card.js
@@ -1,24 +1,31 @@
 import Images from './Images';
 import React, { useState, useEffect } from 'react'
+import Bio from './Bio'
 
 function Card({character}) {
 
     const [image, setImage] = useState("")
-    const [popup, setPopup] = useState("")
+    const [popup, setPopup] = useState(null)
 
     useEffect(() => {
         setImage(Images[`${character.id}`])
     })
 
     const openCard = () => {
-        setPopup(character.id)
-        window.alert(character.bio)
+        // debugger
+        setPopup(`${character.id}`)
+        // popup === e.target.value ? setPopup(character) 
+        // setPopup(character.id)
+        
     }
 
 
 
     return (
         <div className="card" id={character.id} onClick={openCard}>
+            {/* {console.log(popup.id)} */}
+            {/* {console.log(character.id)} */}
+            {popup === character.id ? <Bio className="bio" props={character} /> : null }
             <h1 className="card__name">{character.name}</h1>
             <img src={image} alt={`an image of ${character.name}`} className="card__img"/>
             <p className="card__attack">attack: {character.attack}</p>
