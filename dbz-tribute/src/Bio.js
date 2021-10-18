db6d09553b0efbd4ea7a9bbb0376e6ed6bfa1178
Jason Herrera <baytownjason@yahoo.com>
2021-10-17 19:42:25 -0500

    pop up & close success

--- dbz-tribute/src/Bio.js
+++ dbz-tribute/src/Bio.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const Bio = ({props}) => {
+const Bio = ({props, image, openCard}) => {
     // debugger
     // console.log(props.bio)
     return(
     <div className="Bio__Popup">
-        <h1>{props.name}</h1>
-        <p>{props.bio}</p>
+        <p className="Bio__close--button" onClick={openCard}>X</p>
+        <img src={image} className="Popup__img" />
+        <h1 className="Popup_title">{props.name}</h1>
+        <p className="Popup_bio">{props.bio}</p>
+        <br></br>
+        <h2>Abilities:</h2>
+        <ol className="Popup_abilities">
+            {props.abilities.map(ability => 
+            <li key={ability}>
+                {ability}
+            </li>)
+            }
+        </ol>
+        
     </div>
     )
 }
--- dbz-tribute/src/Card.js
+++ dbz-tribute/src/Card.js
@@ -13,7 +13,8 @@ function Card({character}) {
 
     const openCard = () => {
         // debugger
-        setPopup(`${character.id}`)
+        popup === null ? setPopup(`${character.id}`) : setPopup(null)
+        
         // popup === e.target.value ? setPopup(character) 
         // setPopup(character.id)
         
@@ -25,7 +26,7 @@ function Card({character}) {
         <div className="card" id={character.id} onClick={openCard}>
             {/* {console.log(popup.id)} */}
             {/* {console.log(character.id)} */}
-            {popup === character.id ? <Bio className="bio" props={character} /> : null }
+            {popup === character.id ? <Bio openCard={openCard} className="bio" image={image} props={character} /> : null }
             <h1 className="card__name">{character.name}</h1>
             <img src={image} alt={`an image of ${character.name}`} className="card__img"/>
             <p className="card__attack">attack: {character.attack}</p>
